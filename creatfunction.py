e113a1a0be5cb06e5a05acfbdc84305e6042c4ee
Md Shoaib Islam <60780232+Shoaib3375@users.noreply.github.com>
2020-06-16 15:19:01 +0600

    Createfunction.py
    How to create a function .

---
+++ creatfunction.py
@@ -0,0 +1,10 @@
+# Function!
+def add_two_values(first, second):
+    return first + second
+
+number_1 = 10
+number_2 = 3
+sum = add_two_values(number_1, number_2)
+
+# print(f"{number_1} + {number_2} = {sum}")
+print(f"{number_1} + {number_2} = {number_1 + number_2}") 
